4fbea4b9a3c93e477efbe6e98e25bb5eb8c9ea00
albamaister <bryanalba92@gmail.com>
2022-03-25 09:21:50 -0500

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ef04557deb16d84"; }
+/******/ 		__webpack_require__.h = function() { return "39f8e7a535156dce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.229d193fce6dc9a0.hot-update.js
+++ .next/static/webpack/webpack.229d193fce6dc9a0.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5ef04557deb16d84"; }
+/******/ 	__webpack_require__.h = function() { return "39f8e7a535156dce"; }
 /******/ }();
 /******/ 
 /******/ }
